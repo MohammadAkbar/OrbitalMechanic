2c91d03ebb2bf442d7f487eb4716472986e5bfbd
MoAkbar <makba002@ucr.edu>
2020-08-11 07:43:21 -0500

    get_TLEs.py
    added get_TLEs function

--- get_TLEs.py
+++ get_TLEs.py
@@ -1,4 +1,31 @@
-class TLE:
-    def __init__( line_1 , line_2 ):
-        self.line_1 = line_1
-        self.line_2 = line_2
+def get_TLEs(     NORADid=26073,
+                    spacetrack_user,
+                    spacetrack_pass,
+                    epoch="2010-01-01--2011-01-01",
+                    stepsize=1
+    ):
+
+    import spacetrack.operators as op
+    from spacetrack import SpaceTrackClient
+    import numpy as np
+    import pandas as pd
+    from io import StringIO
+
+    def mycallback(until):
+        duration = int(round(until - time.time()))
+
+    st = SpaceTrackClient(spacetrack_user,spacetrack_pass)
+    st.callback = mycallback
+
+    tles = st.tle( norad_cat_id=NORADid , epoch=epoch , format='csv' )
+    if tles:
+        T = pd.read_csv( StringIO(tles) )
+    T.sort_values( by=['EPOCH'] )
+    Ts = list(
+        map(
+            lambda b1,b2,a1,a2:
+                [a1,a2,b1,b2],
+                T["TLE_LINE1"][stepsize:] , T["TLE_LINE2"][stepsize:] , T["TLE_LINE1"][:-stepsize] , T["TLE_LINE2"][:-stepsize]
+        )
+    )
+    return Ts
