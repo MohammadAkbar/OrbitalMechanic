b8bd72cd2088a295df9c84e892f94fe25ba86b1c
MoAkbar <makba002@ucr.edu>
2020-08-11 12:37:46 -0500

    get_batch_TLEs
    getting TLEs for multiple objects at once

--- pullData/get_Data.py
+++ pullData/get_Data.py
@@ -1,5 +1,5 @@
 # generate TLE pairs for object(NORADid)
-def get_batch_TLEs(NORADid,
+def get_TLEs(NORADid,
             spacetrack_user,
             spacetrack_pass,
             epoch="2010-01-01--2011-01-01",
@@ -31,6 +31,35 @@ def get_batch_TLEs(NORADid,
         )
     )
     return Ts
+
+def get_batch_TLEs(NORADid,
+            spacetrack_user,
+            spacetrack_pass,
+            epoch="2010-01-01--2011-01-01",
+            stepsize=1
+            ):
+
+    import spacetrack.operators as op
+    from spacetrack import SpaceTrackClient
+    import numpy as np
+    import pandas as pd
+    from io import StringIO
+
+    def mycallback(until):
+        duration = int(round(until - time.time()))
+
+    st = SpaceTrackClient(spacetrack_user,spacetrack_pass)
+    st.callback = mycallback
+
+    TLE_ids = {}
+    tles = st.tle( norad_cat_id=NORADid , epoch=epoch , format='csv' )
+    if tles:
+      t = pd.read_csv(StringIO(tles))
+      for id in NORADid:
+        ids = t[(t['NORAD_CAT_ID'] == id)]
+        TLE_ids[id] = ids[['NORAD_CAT_ID','TLE_LINE1','TLE_LINE2']]
+    return TLE_ids
+
 
 #Get Current MEO objects
 def get_MEO_ids():
