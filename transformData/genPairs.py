3961ba757216f101f83b0874ead9c7d572282dd8
MoAkbar <makba002@ucr.edu>
2020-08-12 15:44:13 -0500

    added gen_all_pairs
    data transfromation

--- transformData/genPairs.py
+++ transformData/genPairs.py
@@ -1,3 +1,19 @@
-def genPairs(TLEs_df,stepsize=1):
-    print("test")
-    return 0
+def genPairs(T,stepsize=1):
+  import pandas as pd
+  Ts = list(
+        map(
+            lambda b1,b2,a1,a2:
+                [a1,a2,b1,b2],
+                T["TLE_LINE1"][stepsize:] , T["TLE_LINE2"][stepsize:] , T["TLE_LINE1"][:-stepsize] , T["TLE_LINE2"][:-stepsize]
+        )
+    )
+  Ts_df = pd.DataFrame(Ts,columns=["TLE_A_LINE1","TLE_A_LINE2","TLE_B_LINE1","TLE_B_LINE2"])
+  #helpers.prettyPrint(Ts_df)
+  return Ts_df
+
+def gen_all_pairs(TLE_pairs_ids):
+  all_pairs = {}
+  for key, satellite_object in TLE_ids_LEO.items():
+    pairs = genPairs(satellite_object)
+    all_pairs[key] = pairs
+  return all_pairs
